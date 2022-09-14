c0d6f23796288ad0d72a0166ef8f5b20029f1e96
Shailesh Gore <shailesh.gore@infostretch.com>
2022-09-14 10:28:19 +0530

    new react project

---
+++ src/Components/Textform.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react'
+
+const Textform = (props) => {
+    const[text, setText]=useState("");
+
+    const onClickHandler=()=>{
+        
+        let upperCaseText  = text.toUpperCase()
+        setText(upperCaseText);
+    }
+
+    const onClicklowHandler=()=>{
+        
+        let LowerCaseText  = text.toLowerCase()
+        setText(LowerCaseText);
+    }
+
+    const onClickClearHandler=()=>{
+        
+        let Clear  = ''
+        setText(Clear);
+    }
+    
+    const onChangeHandler=(event)=>{
+        
+        setText(event.target.value)
+    }
+    return (
+        <>
+        <div>
+            <h1 className='my-4'>{props.heading}</h1>
+            <div className="mb-3">
+
+                <textarea className="form-control" value={text} onChange={onChangeHandler} id="exampleFormControlTextarea1" rows="8"></textarea>
+            </div>
+
+            <button className='btn btn-primary' onClick={onClickHandler}>Change to UpperCase</button>
+            <button className='mx-4 btn btn-primary' onClick={onClicklowHandler}>Change to LowerCase</button>
+            <button className='mx-4 btn btn-primary' onClick={onClickClearHandler}>Clear</button>
+        </div>
+
+        <div className="container">
+            <h1>Letter Summary</h1>
+            <h5>{text.split(" ").length} words and {text.length} character</h5>
+            <h5>Time required to read is {0.0008*text.split(" ").length}</h5>
+            <h2>Preview</h2>
+            <p>{text}</p>
+        </div>
+
+        
+        </>
+    )
+}
+
+export default Textform
